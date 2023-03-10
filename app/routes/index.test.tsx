1f8db3cb093b532c85c77f00388591bbdb62a52e
Jason Silvers <jason.silvers.2@us.af.mil>
2023-03-09 14:27:43 -1000

    fix test

--- app/routes/index.test.tsx
+++ app/routes/index.test.tsx
@@ -1,9 +1,14 @@
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import IndexPage from "./index";
+import { MemoryRouter } from "react-router";
 
 test("the index page loads", () => {
-  const screen = render(<IndexPage />);
+  const screen = render(
+    <MemoryRouter>
+      <IndexPage />
+    </MemoryRouter>
+  );
 
   const infosection = screen.getByText(
     /An AI assistant that helps you write Air Force narrative style bullets/i
